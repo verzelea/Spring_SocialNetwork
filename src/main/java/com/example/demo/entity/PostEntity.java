27b8d523a86459f697c15e7a8839f023131555b1
Galimede <mat.degand@outlook.fr>
2020-12-12 21:08:11 +0100

    :fire: Get rid of getter/setter as we have lombok

--- src/main/java/com/example/demo/entity/PostEntity.java
+++ src/main/java/com/example/demo/entity/PostEntity.java
@@ -16,35 +16,4 @@ public class PostEntity {
 
     public PostEntity(){}
 
-    public int getId_post() {
-        return post_id;
-    }
-
-    public void setId_post(int post_id) {
-        this.post_id = post_id;
-    }
-
-    public String gettext() {
-        return text;
-    }
-
-    public void settext(String text) {
-        this.text = text;
-    }
-
-    public int getuser_id() {
-        return user_id;
-    }
-
-    public void setuser_id(int user_id) {
-        this.user_id = user_id;
-    }
-
-    public boolean getprivate_post() {
-        return private_post;
-    }
-
-    public void setprivate_post(boolean private_post) {
-        this.private_post = private_post;
-    }
 }
