5da116dba0a53f6c82d54213cde4c13668bba861
Francois Lannoy <flannoy154@gmail.com>
2020-12-12 21:22:18 +0100

    :art: changed paths of both posts and users controllers

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "/api")
+@RequestMapping(path = "/users")
 public class UserController {
 
     @Autowired
