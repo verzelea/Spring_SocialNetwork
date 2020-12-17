848ce148540295ea8979509bf194044639254936
Francois Lannoy <flannoy154@gmail.com>
2020-12-17 18:28:01 +0100

    :eyes: deleting... something

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,7 +5,6 @@ INSERT INTO User (user_id,username, nom, prenom, password, birthday, description
 INSERT INTO User (user_id,username, nom, prenom, password, birthday, description) VALUES (5, 'user5','gaulois', 'francois', '0000', '2003-01-23', 'love pancake');
 
 INSERT INTO Post (post_id, text, user_id, private_post) VALUES (1, 'Hello', 1, 0);
-INSERT INTO Post (post_id, text, user_id, private_post) VALUES (2, 'Neel sent le babouin', 3, 1);
 INSERT INTO Post (post_id, text, user_id, private_post) VALUES (3, 'J aime les cookies', 4, 0);
 INSERT INTO Post (post_id, text, user_id, private_post) VALUES (4, 'DITES NON AU MANIDESTATION', 2, 0);
 
