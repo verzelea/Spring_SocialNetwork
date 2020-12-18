c9a1dd69f85101ec13198ee6037dd8fbf260731e
Galimede <mat.degand@outlook.fr>
2020-12-18 15:15:58 +0100

    :memo: Add "methods.json" api collection file

--- src/main/java/com/example/demo/entity/UserEntity.java
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -20,6 +20,7 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userId;
+    @Column(unique=true)
     private String username;
     private String nom;
     private String prenom;
